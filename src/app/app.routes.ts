8992710be21acca761161d2bed0b5a485a7c1952
Coco10023 <mauricio2003@live.se>
2026-06-03 22:31:11 +0200

    Skapar routes för kurs och schema komponent sidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Courses } from "./pages/courses/courses";
+import { Schedule } from "./pages/schedule/schedule";
+
+// Kopplar routing filen till komponenter för att användare ska kunna gå mellan kurslistan och ramschemat. 
+export const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'courses', component: Courses },
+  { path: 'schedule', component: Schedule }
+];
