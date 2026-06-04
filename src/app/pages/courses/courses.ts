2d0a4061c7ea806e88fd542c57c38a517a0694a2
Coco10023 <mauricio2003@live.se>
2026-06-04 14:10:55 +0200

    Skriver kod för att visa kurslistan på Courses sidan

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../../services/course';
+import { Course } from '../../models/course';
 
 @Component({
   selector: 'app-courses',
   imports: [],
   templateUrl: './courses.html',
-  styleUrl: './courses.css',
+  styleUrl: './courses.css'
 })
-export class Courses {}
+export class Courses implements OnInit {
+  courses: Course[] = [];
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.courseService.getCourses().subscribe((data) => {
+      this.courses = data;
+    });
+  }
+}
