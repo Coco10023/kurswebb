2fdcc719bb109954e360ffedf3432e91a20cec02
Coco10023 <mauricio2003@live.se>
2026-06-06 10:10:19 +0200

    Bygger schema sidan som även visar antal poäng

--- src/app/pages/schedule/schedule.ts
+++ src/app/pages/schedule/schedule.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Course } from '../../models/course';
+import { ScheduleService } from '../../services/schedule';
 
 @Component({
   selector: 'app-schedule',
+  standalone: true,
   imports: [],
   templateUrl: './schedule.html',
-  styleUrl: './schedule.css',
+  styleUrl: './schedule.css'
 })
-export class Schedule {}
+export class Schedule {
+  constructor(private scheduleService: ScheduleService) {}
+
+  get schedule(): Course[] {
+    return this.scheduleService.getSchedule();
+  }
+
+  get totalPoints(): number {
+    return this.scheduleService.getTotalPoints();
+  }
+
+  removeCourse(courseCode: string): void {
+    this.scheduleService.removeCourse(courseCode);
+  }
+}
